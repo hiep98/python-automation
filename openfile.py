bfea3bc386a50d3c6458ab4f1fb5e291e6a77d8f
Nguyen Thanh Hiep <43906825+hiep98@users.noreply.github.com>
2022-11-29 21:12:57 +0700

    Add files via upload

---
+++ openfile.py
@@ -0,0 +1,9 @@
+
+##import sys, string, os
+##os.system("C:/Users/Victim/Downloads/test.exe")
+
+import subprocess
+##subprocess.Popen([r'C:/Users/Victim/Downloads/test.exe'])
+
+if
+subprocess.call(["C:/Users/Victim/Downloads/test.exe"])
